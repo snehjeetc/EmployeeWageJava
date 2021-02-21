cc80b2930efdf53a464cf4b13727e4da35b49902
snehjeetc12 <csnehjeet12@gmail.com>
2021-02-21 11:47:45 +0530

    Employee Wage- first commit

--- EmpMain.java
+++ EmpMain.java
@@ -5,6 +5,8 @@ public class EmpMain {
 
 	public static void main(String[] args) {
 				Employee e = new Employee();
+				
+				System.out.println("Welcome to Employee Wage Computation Program");
 				if(e.checkAttendance()==1)
 					System.out.println("Employee is present");
 				else
