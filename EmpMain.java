87ca790f60929788cd24a6a26a1f16257ad7ed10
snehjeetc12 <csnehjeet12@gmail.com>
2021-02-23 23:44:42 +0530

    Employee Wage using OOPs concept

--- EmpMain.java
+++ EmpMain.java
@@ -4,12 +4,19 @@ import com.employee.Employee;
 public class EmpMain {
 
 	public static void main(String[] args) {
-				Employee e = new Employee();
+                int Dmart_WagePerHr = 200;
+                int Dmart_FullDayHr = 10;
+                int Dmart_PartTimeHr = 6;
+                int Dmart_workDaysLimit = 26;
+                int Dmart_workHrsLimit = 130;
+
+				Employee Dmart = new Employee(Dmart_WagePerHr, Dmart_FullDayHr,
+                       Dmart_PartTimeHr, Dmart_workDaysLimit, Dmart_workHrsLimit );
 				
 				System.out.println("Welcome to Employee Wage Computation Program");
-                while(!e.workingDayExceeded() && !e.workingHrExceeded())    
-                    e.calculateDailyWage();
-                System.out.println("Monthly wage= " + e.salary);
+                while(!Dmart.workingDayExceeded() && !Dmart.workingHrExceeded())    
+                    Dmart.calculateDailyWage();
+                System.out.println("Monthly wage of Dmart Employee = " + Dmart.salary);
 	}
 
 }
--- Employee.java
+++ Employee.java
@@ -4,56 +4,66 @@ import java.util.Random;
 public class Employee {
 	static final short IS_FULL_TIME = 1;
 	static final short IS_PART_TIME = 2;
+    static final short IS_PRESENT = 1;
 	
-	static int WAGE_PER_HR;
-	static int FULL_DAY;
-	static int PART_TIME_HR;
+	int WAGE_PER_HR;
+	int FULL_DAY;
+	int PART_TIME_HR;
     
-	static int TOT_WORKDAYS_LIMIT;
-	static int TOT_WORKING_HRS_LIMIT;
+	int TOT_WORKDAYS_LIMIT;
+	int TOT_WORKING_HRS_LIMIT;
 
 	int hrsWorked;
 	int totDaysWorked;
 	int dailyPayment;
     int salary;
-
-	static {
-		FULL_DAY = 8;
-		PART_TIME_HR = 4;
-		
-		WAGE_PER_HR = 20;
-		TOT_WORKDAYS_LIMIT = 20;
-		TOT_WORKING_HRS_LIMIT = 100;
-	}
 	
-	Employee(){
+	Employee(int wagePerHr, int FullDayHr, int PartTimeHr, int workDaysLimit, 
+            int workHrsLimit){
 		dailyPayment = 0;
 		hrsWorked = 0;
 		totDaysWorked = 0;
         salary = 0;
+        WAGE_PER_HR = wagePerHr;
+        FULL_DAY = FullDayHr;
+        PART_TIME_HR = PartTimeHr;
+        TOT_WORKDAYS_LIMIT = workDaysLimit;
+        TOT_WORKING_HRS_LIMIT = workHrsLimit;
+	}
+    
+    public int checkAttendance() {
+		short empCheck = (short)((Math.random() * 10)%2);
+		if(empCheck == IS_PRESENT)
+			return 1;
+		else
+			return 0;
 	}
 
 	public void calculateDailyWage() {
         Random rand = new Random();
-		int empCheck= rand.nextInt(3);
-		switch(empCheck) {
-		case IS_FULL_TIME:	
-				System.out.println("Employee is present");
-				dailyPayment = FULL_DAY * WAGE_PER_HR;
-                hrsWorked += FULL_DAY;
-                totDaysWorked++;
-				break;
-		case IS_PART_TIME:
-				System.out.println("Employee is present");
-				dailyPayment = PART_TIME_HR* WAGE_PER_HR;
-                hrsWorked += PART_TIME_HR;
-                totDaysWorked++;
-				break;
-		default:
-				System.out.println("Employee is absent");
-				dailyPayment = 0;
-				break;
-		}
+		int empCheck= checkAttendance();
+        if(empCheck == 1){
+            int jobType = rand.nextInt(2) + 1;
+		    switch(jobType) {
+		        case IS_FULL_TIME:	
+				        System.out.println("Employee is present");
+				        dailyPayment = FULL_DAY * WAGE_PER_HR;
+                        hrsWorked += FULL_DAY;
+                        totDaysWorked++;
+				        break;
+		        case IS_PART_TIME:
+				        System.out.println("Employee is present");
+				        dailyPayment = PART_TIME_HR* WAGE_PER_HR;
+                        hrsWorked += PART_TIME_HR;
+                        totDaysWorked++;
+				        break;
+		
+		        }
+        }
+        else{
+            System.out.println("Employee is absent");
+            dailyPayment = 0;
+        }
         	salary += dailyPayment;
 		System.out.println("Daily Payment: " + dailyPayment);
     }
